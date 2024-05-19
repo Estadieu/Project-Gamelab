1a2ca727d331f8e5a97c821863e6e19b01051edc
Geoffrey Naji <geoffrey.naji@etud.univ-jfc.fr>
2024-05-19 16:52:38 +0200

    Add Songs

--- Scenes/Level/Entities/Enemy/explosion.gd
+++ Scenes/Level/Entities/Enemy/explosion.gd
@@ -1,10 +1,12 @@
 extends Sprite2D
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$AnimationPlayer.play("explode")
 
 
 func _on_animation_player_animation_finished(_anim_name):
+	visible = false
+
+func _on_sound_finished():
 	queue_free()
